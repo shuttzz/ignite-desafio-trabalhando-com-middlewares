af53a747c8a05f433ab86cb4f61ca950e1a7ea02
José Assis de Menezes Neto <jose.neto@hitcode.com.br>
2021-05-30 23:46:38 -0300

    Fim do desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+
+  if (!userExists) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  // plano grátis e ainda não possuir 10 todos cadastrados ou se ele já estiver com o plano Pro ativado. 
+  const availability = (!user.pro && user.todos.length < 10) || user.pro;
+
+  if (!availability) {
+    return response.status(403).json({ error: "User have to update the plan" });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const isUUID = validate(id);
+
+  if (!isUUID) {
+    return response.status(400).json({ error: "ID is not UUID" });
+  }
+
+  const userExists = users.find(user => user.username === username);
+
+  if (!userExists) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  const todoExists = userExists.todos.find(todo => todo.id === id);
+
+  if (!todoExists) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  request.user = userExists;
+  request.todo = todoExists;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.id === id);
+
+  if (!userExists) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
